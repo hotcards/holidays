b8c0c9bed6ffe6a1898cb54f3414c7fa7dcc17d6
James Gifford <james@armyofminions.com>
2016-12-19 11:54:18 -0500

    ONE MORE TIME!
    https://www.youtube.com/watch?v=FGBhQbmPwH8&feature=youtu.be&t=29

--- lib/holidays/hotcards.rb
+++ lib/holidays/hotcards.rb
@@ -18,7 +18,7 @@ module Holidays
 
     def self.holidays_by_month
       {
-              1 => [{:mday => 1, :observed => lambda { |date| Holidays.to_weekday_if_weekend(date) }, :observed_id => "to_weekday_if_weekend", :name => "New Year's Day", :regions => [:hotcards]}],
+              1 => [{:wday => 1, :week => , :name => "New Year's Day", :regions => [:hotcards]}],
       5 => [{:wday => 1, :week => -1, :name => "Memorial Day", :regions => [:hotcards]}],
       7 => [{:mday => 4, :observed => lambda { |date| Holidays.to_weekday_if_weekend(date) }, :observed_id => "to_weekday_if_weekend", :name => "Independence Day", :regions => [:hotcards]}],
       9 => [{:wday => 1, :week => 1, :name => "Labor Day", :regions => [:hotcards]}],
