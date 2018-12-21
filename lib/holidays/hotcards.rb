7d0ab2c4e79ba6d6e0103d0a1c11ff650fa1bee6
Will Yaworsky <will.yaworsky@gmail.com>
2018-12-20 23:59:58 -0500

    Update hotcards.rb

--- lib/holidays/hotcards.rb
+++ lib/holidays/hotcards.rb
@@ -25,7 +25,12 @@ module Holidays
       9 => [{:wday => 1, :week => 1, :name => "Labor Day", :regions => [:hotcards]}],
       11 => [{:wday => 4, :week => 4, :name => "Thanksgiving", :regions => [:hotcards]},
             {:wday => 5, :week => 4, :name => "Day After Thanksgiving", :regions => [:hotcards]}],
-      12 => [{:mday => 25, :observed => lambda { |date| Holidays.to_weekday_if_weekend(date) }, :observed_id => "to_weekday_if_weekend", :name => "Christmas Day", :regions => [:hotcards]}]
+      12 => [
+        {:mday => 25, :observed => lambda { |date| Holidays.to_weekday_if_weekend(date) }, :observed_id => "to_weekday_if_weekend", :name => "Christmas Day", :regions => [:hotcards]},
+
+          {:mday => 24, :observed => lambda { |date| Holidays.to_weekday_if_weekend(date) }, :observed_id => "to_weekday_if_weekend", :name => "Christmas Day", :regions => [:hotcards]}
+
+      ]
       }
     end
   end
